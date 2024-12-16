b91fb557e55a48f4c0cf6e0624ab4b70dab7b485
kerim-kaplaner-vinci <kerim.kaplaner@student.vinci.be>
2024-12-16 23:14:03 +0100

    test routing

---
+++ tutorials/front/routing/src/main.tsx
@@ -0,0 +1,37 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { AboutPage, ContactPage, HomePage, UserPage } from "./App.tsx";
+import App from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "about",
+        element: <AboutPage />,
+      },
+      {
+        path: "contact",
+        element: <ContactPage />,
+      },
+      {
+        path: "users/:userId",
+        element: <UserPage />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
